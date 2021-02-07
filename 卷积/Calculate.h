e645df60e6792d249e810fef68e79f8a784c891f
张 哲城 <junguan_quanli@outlook.com>
2021-02-07 22:37:43 +0800

    111

--- 卷积/Calculate.h
+++ 卷积/Calculate.h
@@ -2,6 +2,12 @@
 #include<iostream>
 #include<vector>
 #include "InputFormat.h"
+struct TryTimes
+{
+public:
+	int horizontalTimes;//横向需要计算的次数
+	int verticalTimes;//纵向需要计算的次数
+};
 /**
  * @brief 通过数组卷积来计算向量（二维）
  * @param inputVector    输入数组，从左至右，从上到下拆散排列
@@ -10,11 +16,44 @@
  * @return 返回说明
  *     返回一个数组
  */
-std::vector<int> Calculate(InputVector inputVector,InputConvolutionkernel inputConvolutionkernel)
+
+std::vector<int> Calculate(InputVector& inputVector,InputConvolutionkernel& inputConvolutionkernel)
 {
 	std::vector<int> FormatedInputVector, FormatedInputConvolutionkernel;
-	for (long i = 0; i < (inputVector.lines - 1);i++)
-	{
 
+	int VectorNumberEveryLine;//向量每行的长度 当为2行且个数为奇数时在整除后需要加1
+	int ConvolutionkernelNumberEveryLine;//卷积核每行的长度 当为2行且个数为奇数时在整除后需要加1
+
+	TryTimes CalculateTimes;//横纵向需要计算的次数
+
+	//算出每行卷积与卷积核的长度
+	VectorNumberEveryLine = inputVector.InputVector.size() / 2;
+	ConvolutionkernelNumberEveryLine = inputConvolutionkernel.InputVector.size() / 2;
+
+	//当卷积为2行且个数为奇数时，在整除后需要加1
+	if (inputVector.lines == 2 && inputVector.InputVector.size() % 2 == 1)
+	{
+		VectorNumberEveryLine += 1;
+	}
+	if (inputConvolutionkernel.lines == 2 && inputConvolutionkernel.InputVector.size() % 2 == 1)
+	{
+		ConvolutionkernelNumberEveryLine += 1;
+	}
+
+	//算出横向纵向所需要卷积的次数
+	CalculateTimes.horizontalTimes = VectorNumberEveryLine - ConvolutionkernelNumberEveryLine + 1;
+	CalculateTimes.verticalTimes = inputVector.lines - inputConvolutionkernel.lines + 1;
+
+	//算出卷积与卷积核的总大小
+	int VectorTotalNumber = VectorNumberEveryLine * inputVector.lines;
+	int ConvolutionkernelTotalNumber = ConvolutionkernelNumberEveryLine * inputConvolutionkernel.lines;
+
+	//重新转为二维数组并补零
+	for (long i = 0; i < inputVector.lines;i++)
+	{
+		for (long j = 0; i < VectorNumberEveryLine; i++)
+		{
+			FormatedInputVector[i][j] = inputVector.InputVector[]
+		}
 	}
 }
