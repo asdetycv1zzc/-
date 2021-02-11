37456f0afb63ee2fefb7a290826b7aa964402935
张 哲城 <junguan_quanli@outlook.com>
2021-02-11 17:15:14 +0800

    111

--- 卷积/Calculate.h
+++ 卷积/Calculate.h
@@ -3,14 +3,33 @@
 #include<exception>
 #include<vector>
 #include "InputFormat.h"
+#include "OutputFormat.h"
 
-std::vector<std::vector<int>> ConvolutionCalculate(InputCalculateVector inputVector, InputCalculateConvolutionkernel inputConvolutionkernel)
+CalculatedVector ConvolutionCalculate(InputCalculateVector inputVector, InputCalculateConvolutionkernel inputConvolutionkernel)
 {
 	TryTimes CalculateTimes;//横纵向需要计算的次数
 
 	CalculateTimes.horizontalTimes = inputVector.VectorNumberEveryLine - inputVector.VectorNumberEveryLine + 1;
 	CalculateTimes.verticalTimes = inputVector.lines - inputConvolutionkernel.lines + 1;
 
+	//初始化结果
+	CalculatedVector result;
+	result.HorizontalLength = CalculateTimes.horizontalTimes;
+	result.VerticalLength = CalculateTimes.verticalTimes;
+	
+	//开辟Vector的纵向空间
+	result.Vector.resize(result.VerticalLength);
 
-	return NULL;
+	//开辟Vector的横向空间
+	for (long i = 0;i < result.VerticalLength;i++)
+	{
+		result.Vector[i].resize(result.HorizontalLength);
+	}
+
+	//开始进行横向卷积计算
+	for (long i = 0;i < CalculateTimes.horizontalTimes;i++)
+	{
+		
+	}
+	return result;
 }
---
+++ 卷积/OutputFormat.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+struct CalculatedVector
+{
+public:
+	long VerticalLength;
+	long HorizontalLength;
+	std::vector<std::vector<int>> Vector;
+};
